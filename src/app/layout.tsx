2877e4d334481f0abba4898faf6f3a3fe23189bb
nelsonelaye <nelsonelaye@gmail.com>
2025-07-02 07:40:32 +0100

    refactor: return client directive to Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import TanstackClientProvider from "./TanstackClientProvider";
 import { ThemeProvider } from "./ThemeProvider";
